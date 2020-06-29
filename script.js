6459169a13f88b9b6475fefd4dcc594fadaeec76
hilbug <65197724+hilbug@users.noreply.github.com>
2020-06-29 17:36:21 -0400

    push files to git

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// Create arrays for lowercase letters, uppercase letters, numbers, special characters
+
+// Create user prompts for each question
+  // length of password between 8 and 128
+  // lower case letters
+  // uppercase letters
+  // numbers
+  // special characters
+// validate the user has chosen at least one character type
+// when answered, a password is generated matching selected criteria
+// password displayed in an alert OR written to the page
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
