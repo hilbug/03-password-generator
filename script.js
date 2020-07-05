0b9954a4c3db5f9dacf93bd91792efcf97feed2f
hilbug <65197724+hilbug@users.noreply.github.com>
2020-07-05 16:45:46 -0400

    add arrays to function

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -7,9 +6,17 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
+}
+
+//Function to generate password based in user inputs
+function generatePassword() {
+// Arrays for password
+  let lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
+  let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+  let numbers = "0123456789".split("");
+  let symbols = "!*$%&#@+?:;".split("");
 }
 
 // Add event listener to generate button
