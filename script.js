d563e573bb94255de68c39316c77aecdded2f88e
hilbug <65197724+hilbug@users.noreply.github.com>
2020-07-05 19:07:10 -0400

    add prompt text

--- script.js
+++ script.js
@@ -12,11 +12,34 @@ function writePassword() {
 
 //Function to generate password based in user inputs
 function generatePassword() {
-// Arrays for password
+
+  // prompt user for password length
+  let userLength = prompt("Enter a password length between 8 and 128.");
+  
+  // If not a number between 8 and 128, alert user
+  if (userLength < 8 || userLength > 128 || isNaN(userLength)) {
+    alert("Please enter a valid selection for password length.");
+    return;
+  };
+
+  //prompt user for lower case letters
+  let userLowerCase = confirm("Would you like LOWER CASE letters in your password?");
+
+  //prompt user for upper case letters
+  let userUpperCase = confirm("Would you like UPPER CASE letters in your password");
+
+  //prompt user for numbers
+  let userNumbers = confirm("Would you like NUMBERS in your password");
+
+  //prompt user for symbols
+  let userSymbols = confirm("Would you like SYMBOLS in your password?");
+
+  // Arrays for password
   let lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
   let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
   let numbers = "0123456789".split("");
   let symbols = "!*$%&#@+?:;".split("");
+  let pwArray = [];
 }
 
 // Add event listener to generate button
