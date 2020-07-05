cc92d29739012a9f1cd9e974114fc22737750cf9
hilbug <65197724+hilbug@users.noreply.github.com>
2020-07-05 19:54:05 -0400

    generate password

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ function generatePassword() {
   let userLength = prompt("Enter a password length between 8 and 128.");
   
   // If not a number between 8 and 128, alert user
+  // I think I need a while loop but can't get it to work.
   if (userLength < 8 || userLength > 128 || isNaN(userLength)) {
     alert("Please enter a valid selection for password length.");
     return;
@@ -34,12 +35,42 @@ function generatePassword() {
   //prompt user for symbols
   let userSymbols = confirm("Would you like SYMBOLS in your password?");
 
-  // Arrays for password
-  let lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
-  let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-  let numbers = "0123456789".split("");
-  let symbols = "!*$%&#@+?:;".split("");
-  let pwArray = [];
+  // Strings for password
+  let lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let numbers = "0123456789";
+  let symbols = "!*$%&#@+?:;()^";
+  let pwString = "";
+
+  // Create pwString based on user inputs
+  if (userLowerCase) {
+    pwString = pwString + lowerCase;
+  }
+
+  if (userUpperCase) {
+    pwString = pwString + upperCase;
+  } 
+
+  if (userNumbers) {
+    pwString = pwString + numbers;
+  }
+
+  if (userSymbols) {
+    pwString = pwString + symbols;
+  }
+
+  console.log(pwString);
+
+  let userPassword = "";
+
+  // generate random string from StackOverflow: 
+  // https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+  for (let i = 0; i < userLength; i++) {
+    userPassword += pwString.charAt(Math.floor(Math.random() * pwString.length));
+  }
+
+  console.log(userPassword);
+  userPWPrompt = alert("Your password is " + userPassword);
 }
 
 // Add event listener to generate button
